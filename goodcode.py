420ca7a6fd21efc1d6fd8b1752da9c73f138b415
Ranvita Kothapalli <rkothap3@charlotte.edu>
2026-02-22 18:34:35 -0500

    Add documentation to the add() method, subtract() method, multiply() method, and te divide() method

--- goodcode.py
+++ goodcode.py
@@ -1,17 +1,25 @@
 class Calculator:
 
+    """" The add method takes two int parameters. The add() method will them
+return the sum of the two ints."""
     @staticmethod
     def add(a, b):
         return a + b
 
+    """ The subtract method takes two int parameters. It will then return the
+    difference of the two ints."""
     @staticmethod
     def subtract(a,b):
         return a - b
 
+    """ The multiple method takes two int parameters. It will then return the 
+    product of the two ints """
     @staticmethod
     def multiply(a,b):
         return a * b
 
+    """ The division method takes two int parameters. It will then return the
+    quotient of the two ints"""
     @staticmethod
     def divide(a,b):
         return a / b
