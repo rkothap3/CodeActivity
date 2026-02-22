f060f9922b36c2e276696bf7d6d45445b878695f
Ranvita Kothapalli <rkothap3@charlotte.edu>
2026-02-22 18:23:14 -0500

    Confirm the methods are static and add comments

--- badcode.py
+++ badcode.py
@@ -1,22 +1,29 @@
 class Calculator:
 
-
-
+#adds ints together
+    @staticmethod
     def add(a, b):
         c = 0
         c += a
         c += b
         return c
+
+#subtracts them
+    @staticmethod
     def subtract(a, b):
         c = a
         d = b
         e = c - d
         return e
+
+    @staticmethod
     def multiply(a, b):
         for i in range (b):
             a += a + a
         return a
 
+#divides them
+    @staticmethod
     def divide(a, b):
        c = a / b
        return c
