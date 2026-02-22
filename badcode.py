aa9d9a3fec52b0d48b5a07d0fa704bfb3c09532a
Ranvita Kothapalli <rkothap3@charlotte.edu>
2026-02-22 18:17:29 -0500

    Add multiple and divide method

--- badcode.py
+++ badcode.py
@@ -2,13 +2,21 @@ class Calculator:
 
 
 
-    def add(self, a, b):
-        c = a
-        d = b
-        e = c + d
-        return e
-    def subtract(self, a, b):
+    def add(a, b):
+        c = 0
+        c += a
+        c += b
+        return c
+    def subtract(a, b):
         c = a
         d = b
         e = c - d
         return e
+    def multiply(a, b):
+        for i in range (b):
+            a += a + a
+        return a
+
+    def divide(a, b):
+       c = a / b
+       return c
