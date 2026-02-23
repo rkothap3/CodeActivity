37e369c954ea5812206ecaebd329a2853c513338
Ranvita Kothapalli <rkothap3@charlotte.edu>
2026-02-22 19:11:38 -0500

    Add description of code and made changes to badcode

--- badcode.py
+++ badcode.py
@@ -3,27 +3,28 @@ class Calculator:
 #adds ints together
     @staticmethod
     def add(a, b):
-        c = 0
+        #defines code
+        c = 0 #sets c to 0
         c += a
         c += b
-        return c
+        return c # return c
 
 #subtracts them
     @staticmethod
     def subtract(a, b):
         c = a
         d = b
-        e = c - d
-        return e
+        e = c - d #subtracts c and d
+        return e # returns e
 
     @staticmethod
     def multiply(a, b):
         for i in range (b):
-            a += a + a
-        return a
+            a += a + a # adds a to itself
+        return a #returns e
 
 #divides them
     @staticmethod
     def divide(a, b):
        c = a / b
-       return c
+       return c #returns e
